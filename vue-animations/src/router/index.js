3c50f0f6fdb2f3181da806b61231dedf45f96079
Brian Nduhiu <briannduhiu@gmail.com>
2023-03-08 07:09:05 +0300

    scafold init

--- vue-animations/src/router/index.js
+++ vue-animations/src/router/index.js
@@ -1,23 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import IntroductionView from '../views/IntroductionView.vue';
+import TransitionComponentView from '../views/TransitionComponentView.vue';
+import TransitionGroupsView from '../views/TransitionGroupsView.vue';
+import ClassBasedAnimationsView from '../views/ClassBasedAnimationsView.vue';
+import StateDrivenAnimationsView from '../views/StateDrivenAnimationsView.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'introduction',
+      component: IntroductionView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/transitionComponent',
+      name: 'transitionComponent',
+      component: TransitionComponentView
+    },
+    {
+      path: '/transitionGroups',
+      name: 'transitionGroups',
+      component: TransitionGroupsView
+    },
+    {
+      path: '/classBasedAnimations',
+      name: 'classBasedAnimations',
+      component: ClassBasedAnimationsView
+    },
+    {
+      path: '/stateDrivenAnimations',
+      name: 'stateDrivenAnimations',
+      component: StateDrivenAnimationsView
     }
   ]
-})
+});
 
-export default router
+export default router;
---
+++ vue-animations/src/views/TransitionComponentView.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container">
+  <div class="header">
+    <h1>Vue JS Animation Techniques</h1>
+  </div>
+  <div class="centerContent">
+    <h1 class="backButton">
+      &lt;
+    </h1>
+    <div class="centerDiv">
+      <h1>transitionComponent</h1>
+      <p>
+        This is a collection of animation techniques that can be used in Vue JS. The techniques are based on the Vue JS documentation and the Vue JS community.
+      </p>
+      <p>
+        The techniques are divided into 3 categories:
+      </p>
+      <ul>
+        <li>Transition</li>
+        <li>Animation</li>
+        <li>Transition Group</li>
+      </ul>
+      <p>
+        Each category has a number of techniques that can be used to animate elements in Vue JS.
+      </p>
+      <p>
+        The techniques are based on the Vue JS documentation and the Vue JS community.
+      </p>
+      <p>
+        The techniques are divided into 3 categories:
+      </p>
+      <ul>
+        <li>Transition</li>
+        <li>Animation</li>
+        <li>Transition Group</li>
+      </ul>
+      <p>
+        Each category has a number of techniques that can be used to animate elements in Vue JS.
+      </p>
+    </div>
+    <h1 class="nextButton">
+      &gt;
+    </h1>
+  </div>
+  <div class="footerNav">
+    <ul class="nav">
+      <li class="navItem">
+        <router-link to="/">introduction</router-link>
+      </li>
+      <li class="navItem">
+        <router-link to="/transitionComponent">transitionComponent</router-link>
+      </li>
+      <li>
+        <router-link to="/transitionGroups">transitionGroups</router-link>
+      </li>
+      <li>
+        <router-link to="/classBasedAnimations">classBasedAnimations</router-link>
+      </li>
+      <li>
+        <router-link to="/stateDrivenAnimations">stateDrivenAnimations</router-link>
+      </li>
+    </ul>
+  </div>
+  </div>
+</template>
+
+<style>
+
+</style>
